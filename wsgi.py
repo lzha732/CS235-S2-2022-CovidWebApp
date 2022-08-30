c9849c26a97bdc39edf3c7f9a970ea0b55578860
lzha732 <lzha732@aucklanduni.ac.nz>
2022-08-30 22:33:22 +1200

    Change to port 6000

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,5 @@ from covid import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, threaded=False)
+    app.run(host='localhost', port=6000, threaded=False)
 
